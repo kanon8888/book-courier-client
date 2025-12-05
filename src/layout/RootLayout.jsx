6900b94b0172067fd31840da8ba705873a51ea1d
kanon8888 <mdrakibhasankanon@gmail.com>
2025-12-06 03:01:11 +0600

    navbar and footer

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../pages/shared/navbar/Navbar';
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
---
+++ src/pages/Home/Bananr/Bananr.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bananr = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bananr;
