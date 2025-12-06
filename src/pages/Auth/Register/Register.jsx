e24d7622810049002b25f4d8f12e91f31dab4d22
kanon8888 <mdrakibhasankanon@gmail.com>
2025-12-07 00:21:33 +0600

    six commit

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -3,24 +3,25 @@ import { useForm } from 'react-hook-form';
 
 const Register = () => {
 
-    const { register } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const handleRegistration = () => {
-
+    const handleRegistration = (data) => {
+        console.log('after register', data);
     }
     return (
         <div>
-            <form onSubmit={handleRegistration}>
+            <form onSubmit={handleSubmit(handleRegistration)}>
 
                 <fieldset className="fieldset">
                     {/**email */}
                     <label className="label">Email</label>
-                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+                    <input type="email" {...register('email', { required: true, })} className="input" placeholder="Email" />
+                    {errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>}
 
                     {/**password */}
 
                     <label className="label">Password</label>
-                    <input type="password" {...register('password')} className="input" placeholder="Password" />
+                    <input type="password" {...register('password', { required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/ })} className="input" placeholder="Password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
                 </fieldset>
