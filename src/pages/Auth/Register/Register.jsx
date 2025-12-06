766fa7f377b5720e7cf591a4d17c03fea0a4f103
kanon8888 <mdrakibhasankanon@gmail.com>
2025-12-07 00:08:07 +0600

    five commit

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+
+    const { register } = useForm();
+
+    const handleRegistration = () => {
+
+    }
     return (
         <div>
-            
+            <form onSubmit={handleRegistration}>
+
+                <fieldset className="fieldset">
+                    {/**email */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+
+                    {/**password */}
+
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password')} className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+
+            </form>
         </div>
     );
 };
